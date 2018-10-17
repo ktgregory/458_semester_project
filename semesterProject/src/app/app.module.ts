994d95b332143348ad3670eb8ffdd01ef21e35aa
Katherine Gregory <Katie@hst000540487um.wifi06.olemiss.edu>
2018-10-17 12:13:54 -0500

    initialized firebase config and generated user, card, and auth services

--- semesterProject/src/app/app.module.ts
+++ semesterProject/src/app/app.module.ts
@@ -19,6 +19,17 @@ import { ForgotpasswordPage } from '../pages/forgotpassword/forgotpassword';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule} from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from '../environment/environment';
+import { UserServiceProvider } from '../providers/user-service/user-service';
+import { CardServiceProvider } from '../providers/card-service/card-service';
+import { AuthServiceProvider } from '../providers/auth-service/auth-service';
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -38,7 +49,11 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebaseConfig, 'seniorProject'),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -60,7 +75,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserServiceProvider,
+    CardServiceProvider,
+    AuthServiceProvider
   ]
 })
 export class AppModule {}
---
+++ semesterProject/src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC_wWyl0pCDNK5QnUa_lWXWWTGCtjMgunw",
+    authDomain: "csci458-semesterproject.firebaseapp.com",
+    databaseURL: "https://csci458-semesterproject.firebaseio.com",
+    projectId: "csci458-semesterproject",
+    storageBucket: "csci458-semesterproject.appspot.com",
+    messagingSenderId: "211406106490"
+  }
+};
